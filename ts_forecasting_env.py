fe5c750b958159aa20f859d91301a5cb6afd8f4f
tiagomateus25 <tiagomfonte@gmail.com>
2023-10-02 22:03:52 +0100

    Corrected

--- ts_forecasting_env.py
+++ ts_forecasting_env.py
@@ -4,7 +4,7 @@ from gym import Env
 from gym.spaces import Box
 import numpy as np
 from typing import Optional
-import csv
+
 
 class ts_forecasting_env(Env):
     def __init__(self, historical_dp=10, trajectory=None, data=None, render_mode: Optional[str] = None):
@@ -72,62 +72,3 @@ class ts_forecasting_env(Env):
             
     def close(self):
         pass
-
-# # Open csv
-# TRAJECTORY = 1
-# HISTORICAL_DP = 25 # historical data points (length of state)
-# SPLIT_RATE = 0.80  # split data into train and test data
-# file = open('allData/traj' + str(TRAJECTORY) + '_allData.csv')
-
-# # Read csv
-# csvreader = csv.reader(file)
-
-# # Store csv data in numpy ndarray
-# rows = []
-# for row in csvreader:
-#     rows.append(row)
-# file.close()
-# data_ = np.array(rows, dtype=np.float64)
-# data_ = np.concatenate(data_)
-
-# # Considering relevant data only
-# if TRAJECTORY == 1:
-#     data_ = data_[5000:14500]
-#     COLOR = '#FF0000'
-
-# # Data split
-# split_index = round(len(data_) * SPLIT_RATE)
-# train_data, test_data = data_[:split_index], data_[split_index:]
-
-# # Normalize data
-# max = np.max(data_)
-# min = np.min(data_)
-# TRAIN_DATA = (train_data - min) / (max - min)  
-# TEST_DATA = (test_data - min) / (max - min)
-
-# # Call environment
-# env = ts_forecasting_env(historical_dp=HISTORICAL_DP, data=TRAIN_DATA)
-
-# ############################## Define training parameters ###############################
-# EPISODES = 1000
-# MAX_STEPS = 1000
-# #########################################################################################
-
-# # Train the agent 
-# reward_history = []
-# average_reward_history = []
-# episode_list = []
-
-# for i in range(1, EPISODES + 1):
-#     obs = env.reset()
-#     done = False
-#     reward = 0
-
-#     for step in range(MAX_STEPS):
-#         act = env.action_space.shape[0]
-#         new_state, step_reward, done, info = env.step(act)
-#         reward += step_reward
-#         obs = new_state
-#         if done:
-#             break
-#     print("episode: ", i, "reward: ", reward, "steps: ", step)
